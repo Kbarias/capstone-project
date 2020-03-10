866b120f9f2b95a12a777beac8e19143afc5536f
Kiara Barias <33493552+Kbarias@users.noreply.github.com>
2020-03-10 18:41:13 -0400

    updated required necesities

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-//const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash'); //to display message that user was successfully registered
 const session = require('express-session');
 const passport = require('passport');
 
 require('dotenv/config');
-=======
-const bodyParser = require('body-parser');
-//require('dotenv/config');
->>>>>>> 6bc7ee9aa98611a5c310939996174f95b84b7086
 
 //Passport config
 require('./config/passport')(passport);
 
 //Bodyparser
-//app.use(bodyParser.json());
 app.use(express.urlencoded( { extended: false } ));
 
 app.use(expressLayouts);
@@ -60,15 +53,8 @@ const placesRoute = require('./routes/places.js');
 app.use('/places', placesRoute);
 
 
-<<<<<<< HEAD
-=======
-//ROUTES
-app.get('/', (req, res) => {
-    res.send('We are on home');
-});
 
->>>>>>> 6bc7ee9aa98611a5c310939996174f95b84b7086
-//Connection to database
+//Connection to database with hidden credentials
 // mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => 
 //    console.log('Connected to database')
 // );
