cbbb81570a4cde8a1c87b9ef6d0a877ecaf92861
Kiara Barias <barias.kiara@gmail.com>
2020-04-29 13:48:30 -0400

    added joining invited session and deleting

--- controllers/gather.js
+++ controllers/gather.js
@@ -36,7 +36,55 @@ exports.get_gather_page = (req, res) => {
     });
 };
 
-exports.create_gathering_page = (req, res) => {
+exports.join_invited_session = (req, res) => {
+    let userid = req.params.id;
+    Promise.all([
+        SessionMember.findOne({_id:req.params.sessionid, members:{$ne:userid}}).populate('_id'),
+        Session.findOne({_id:req.params.sessionid, organizer:{$ne:userid}})
+    ])
+    .then(results =>{
+        //find the session make sure that I am not already a member or organizer of this session
+        const [found_sm, found_sess] = results;
+
+        if(found_sm && found_sess){
+            SessionMember.findByIdAndUpdate({_id:req.params.sessionid}, {$push:{members: userid}})
+                .then(save_member => {
+                    req.flash('success_msg', 'You have been added to the ' + found_sm._id.name + " session." );
+                    res.redirect('/users/login');
+                })
+        }
+        else{
+            req.flash('error_msg', 'You are already a member or organizer of this session, or this session was deleted.');
+            res.redirect('/users/login');
+        }
+    })
+};
+
+exports.delete_session = (req, res) => {
+    Promise.all([
+        Session.findOneAndUpdate({_id:req.params.sessionid}, {is_deleted:true}),
+        SessionMember.findOneAndUpdate({_id:req.params.sessionid}, {is_deleted:true}).populate('members')
+    ])
+    .then(results => {
+        const [session, members] = results;
+
+        for(var i = 0; i < members.members; i++){
+            mailOptions.to = members.members[i].email;
+            mailOptions.subject = 'Your Agora group has been deleted.';
+            mailOptions.text = 'Hello,\n\n' + 'Your Agora group called ' + session.name + ' was deleted by the organizer.';
+            transporter.sendMail(mailOptions, function (err) {
+                if(err) { 
+                    console.log(err);
+                }
+            });
+        }
+        req.flash('success_msg', 'You have successfully deleted your group. Any existing members were notified via email of the deletion.');
+        res.redirect('/gather/'+ req.params.id +'/' + req.params.member);
+
+    })
+};
+
+exports.create_group_page = (req, res) => {
     let userid = req.params.id.slice(0,-1);
     //find user that are active, not this user, and are not deleted
     const users = UserInfo.find({_id:{$ne:userid}, account_status:"Active" , is_deleted:false}).populate('_id');
@@ -50,7 +98,7 @@ exports.create_gathering_page = (req, res) => {
     });
 };
 
-exports.create_group_session = (req, res) => {
+exports.create_group = (req, res) => {
     let userid = req.params.id.slice(0,-1);
     const {session_type, name, description, capacity, person1, person2, person3, person4, person5, person6, person7, person8, person9, person10, place, date, starttime, endtime} = req.body;
 
@@ -119,7 +167,7 @@ exports.create_group_session = (req, res) => {
                 mailOptions.text = 'Hello,\n\n' + 'You were invited to be part of a group called ' + newGroup.name + ' on ' 
                     + newGroup.date + ' at' + newGroup.time.start + ' - ' + newGroup.time.end 
                     + '. The location for this session is ' 
-                    + location.address.full_address + '. Find more information on the Gather page on Agora.';
+                    + location.address.full_address + '. Become a member by clicking the link to be added :\nhttp:\/\/' + req.headers.host + '\/gather'+ '\/invited-join'+ '\/'+ people[i]._id + '\/'+ '\/' + newGroup._id +'.\n';
                 transporter.sendMail(mailOptions, function (err) {
                     if(err) { 
                         console.log(err);
@@ -153,9 +201,104 @@ exports.get_group_session_info = (req, res) => {
 };
 
 exports.join_gathering = (req, res) => {
-    
-    //list the tutors
-    res.render('gather-join', {id: req.params.id ,  member: req.params.member });
+    let userid = req.params.id.slice(0,-1);
+    Promise.all([
+        SessionMember.findOne({_id:req.params.sessionid, members:{$ne:userid}}).populate('_id'),
+        Session.findOne({_id:req.params.sessionid, organizer:{$ne:userid}})
+    ])
+    .then(results =>{
+        //find the group session make sure that I am not already a member or organizer of this session
+        const [found_sm, found_sess] = results;
+
+        if(found_sm && found_sess){
+            SessionMember.findByIdAndUpdate({_id:req.params.sessionid}, {$push:{members: userid}})
+                .then(save_member => {
+                    req.flash('success_msg', 'You have been added to the ' + found_sm._id.name + " group." );
+                    res.redirect('/gather/group-join-info/'+ req.params.id +'/' + req.params.member);
+                })
+        }
+        else{
+            req.flash('error_msg', 'You are already a member or organizer of this group.');
+            res.redirect('/gather/group-join-info/'+ req.params.id +'/' + req.params.member);
+        }
+    })
+
+};
+
+exports.organizer_details = (req, res) => {
+    //get information for the session i am organizer for
+    SessionMember.findOne({_id:req.params.sessionid}).populate('_id').populate({path: '_id', populate: { path: 'organizer', model:'User'}}).populate('members').populate({path: '_id', populate: { path: 'place', model:'Place'}})
+        .then(sessioninfo => {
+            res.render('session_organizer', { id: req.params.id ,member:req.params.member, info:sessioninfo, editing:req.params.edit });
+        })
+};
+
+exports.organizer_edit_details = (req, res) => {
+    const {name, description, date, start, end} = req.body;
+
+    Session.findOne({_id:req.params.sessionid})
+        .then(session => {
+            let up_name, up_description, up_date, up_start, up_end;
+            if(name != null){
+                up_name = name;
+            } else {
+                up_name = session.name;
+            }
+            if(description != null){
+                up_description = description;
+            }else{
+                up_description = session.description;
+            }
+            if(date != null){
+                up_date = date;
+            } else {
+                up_date = session.date;
+            }
+            if(start != null){
+                var time = start.split(':');
+                var hour = (time[0] % 12) || 12;
+                if(time[0]>= 12){ 
+                    up_start = hour + ":" + time[1] + " " + "PM"; 
+                }
+                else{
+                    up_start = hour + ":" + time[1] + " " + "AM";
+                }
+            } else{
+                up_start = session.time.start;
+            }
+            if(end != null){
+                var time = end.split(':');
+                var hour = (time[0] % 12) || 12;
+                if(time[0]>= 12){ 
+                    up_end = hour + ":" + time[1] + " " + "PM"; 
+                }
+                else{
+                    up_end = hour + ":" + time[1] + " " + "AM";
+                }
+            } else{
+                up_end = session.time.end;
+            }
+
+            SessionMember.findOne({_id:req.params.sessionid}).populate('members')
+                .then(info => {
+                    for(var i = 0; i < info.members.length; i ++){
+                        mailOptions.to = info.members[0].email;
+                        mailOptions.subject = 'Your Agora Session has been updated';
+                        mailOptions.text = 'Hello,\n\n' + 'The Agora session ' + info.name + ' has been updated. Please check Agora for the updated details.';
+                        transporter.sendMail(mailOptions, function (err) {
+                            if(err) { 
+                                console.log(err);
+                            }
+                        });
+                    }
+                    Session.findOneAndUpdate({_id:req.params.sessionid}, {name:up_name, description:up_description, date:up_date, 'time.start': up_start, 'time.end': up_end})
+                    .then(updated => {
+                        req.flash('success_msg', 'You have successfully edited your session. Members will be notified.' );
+                        res.redirect('/gather/'+ req.params.id +'/' + req.params.member);
+                    })
+                })
+        })
+
 };
 
 exports.create_tutoring_session = (req, res) => {
@@ -227,7 +370,7 @@ exports.create_tutoring_session = (req, res) => {
                 mailOptions.text = 'Hello,\n\n' + 'You were invited to be part of a tutoring session called ' + newTutoring.name + ' on ' 
                     + newTutoring.date + ' at' + newTutoring.time.start + ' - ' + newTutoring.time.end 
                     + '. The location for this session is ' 
-                    + location.address.full_address + '. Find more information on the Gather page on Agora.';
+                    + location.address.full_address + '. Become a member by clicking the link to be added :\nhttp:\/\/' + req.headers.host + '\/gather'+ '\/invited-join'+ '\/'+ people[i]._id + '\/' + newTutoring._id +'.\n';
                 transporter.sendMail(mailOptions, function (err) {
                     if(err) { 
                         console.log(err);
@@ -248,7 +391,7 @@ exports.create_tutoring_session = (req, res) => {
 
 };
 
-exports.get_tutoring_session_page = (req, res) => {
+exports.get_tutoring_page = (req, res) => {
     let userid = req.params.id.slice(0,-1);
     //find user that are active, not this user, and are not deleted
     const users = UserInfo.find({_id:{$ne:userid}, account_status:"Active" , is_deleted:false}).populate('_id');
@@ -266,21 +409,26 @@ exports.get_tutoring_session_page = (req, res) => {
 
 exports.join_tutoring_session = (req, res) => {
     let userid = req.params.id.slice(0,-1);
-    //find the tutoring session make sure that I am not already a part of this session
-    SessionMember.findOne({_id:req.params.sessionid, members:{$ne:userid}}).populate('_id')
-        .then(found_session => {
-            if(found_session){
-                SessionMember.findByIdAndUpdate({_id:req.params.sessionid}, {$push:{members: userid}})
-                    .then(save_member => {
-                        req.flash('success_msg', 'You have been added to the ' + found_session._id.name + " tutoring session" );
-                        res.redirect('/gather/tutor-join-info/'+ req.params.id +'/' + req.params.member);
-                    })
-            }
-            else{
-                req.flash('error_msg', 'You are already a member or tutor of this tutoring session');
-                res.redirect('/gather/tutor-join-info/'+ req.params.id +'/' + req.params.member);
-            }
-        })
+    Promise.all([
+        SessionMember.findOne({_id:req.params.sessionid, members:{$ne:userid}}).populate('_id'),
+        Session.findOne({_id:req.params.sessionid, organizer:{$ne:userid}})
+    ])
+    .then(results =>{
+        //find the tutoring session make sure that I am not already a member or organizer of this session
+        const [found_sm, found_sess] = results;
+
+        if(found_sm && found_sess){
+            SessionMember.findByIdAndUpdate({_id:req.params.sessionid}, {$push:{members: userid}})
+                .then(save_member => {
+                    req.flash('success_msg', 'You have been added to the ' + found_sm._id.name + " tutoring session." );
+                    res.redirect('/gather/tutor-join-info/'+ req.params.id +'/' + req.params.member);
+                })
+        }
+        else{
+            req.flash('error_msg', 'You are already a member or organizer of this tutoring session.');
+            res.redirect('/gather/tutor-join-info/'+ req.params.id +'/' + req.params.member);
+        }
+    })
         
 };
 
--- routes/gather.js
+++ routes/gather.js
@@ -5,15 +5,23 @@ const auth = require('../config/auth');
 
 router.get('/:id/:member', auth.ensureAuthenticated, GatherController.get_gather_page);
 
-router.get('/create/:id/:member', auth.ensureAuthenticated , GatherController.create_gathering_page);
+router.get('/invited-join/:id/:sessionid', auth.ensureAuthenticated, GatherController.join_invited_session);
 
-router.post('/group-create/:id/:member', auth.ensureAuthenticated, GatherController.create_group_session);
+router.post('/delete/:id/:member/:sessionid', auth.ensureAuthenticated, GatherController.delete_session);
+
+router.get('/create/:id/:member', auth.ensureAuthenticated , GatherController.create_group_page);
+
+router.post('/group-create/:id/:member', auth.ensureAuthenticated, GatherController.create_group);
 
 router.get('/group-join-info/:id/:member/:sessinfo?', auth.ensureAuthenticated, GatherController.get_group_session_info);
 
-router.get('/join/:id/:member', auth.ensureAuthenticated, GatherController.join_gathering);
+router.get('/join/:id/:member/:sessionid', auth.ensureAuthenticated, GatherController.join_gathering);
+
+router.get('/organizer-details/:id/:member/:sessionid/:edit?', auth.ensureAuthenticated, GatherController.organizer_details);
  
-router.get('/tutor-create/:id/:member', auth.ensureAuthenticated, GatherController.get_tutoring_session_page);
+router.post('/organizer-details/edit/:id/:member/:sessionid', auth.ensureAuthenticated, GatherController.organizer_edit_details);
+
+router.get('/tutor-create/:id/:member', auth.ensureAuthenticated, GatherController.get_tutoring_page);
 
 router.post('/tutoring-create/:id/:member', auth.ensureAuthenticated, GatherController.create_tutoring_session);
 
